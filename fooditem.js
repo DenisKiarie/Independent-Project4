4166c373b3424bd6067e1c86fab4ffe9b7a83bb8
DenisKiarie <denis.kiarie@student.moringaschool.com>
2022-01-17 17:23:38 +0300

    added another js file to export to the app.js

---
+++ fooditem.js
@@ -0,0 +1,104 @@
+const foodItem = [
+    {
+        id: 1,
+        fName: "Cheese Pizza",
+        category: "pizza",
+        rating: 4.5,
+        price: 12,
+        img: "/pizza images/Cheese.jpg",
+        quantity: 1
+    },
+
+    {
+        id: 2,
+        fName: "BBQ Pizza",
+        category: "pizza",
+        rating: 4.5,
+        price: 15,
+        img: "/pizza images/BBQ.jpg",
+        quantity: 1
+    },
+
+    {
+        id: 3,
+        fName: "Buffalo Pizza",
+        category: "pizza",
+        rating: 4.5,
+        price: 9,
+        img: "/pizza images/Buffalo.jpg",
+        quantity: 1
+    },
+
+    {
+        id: 4,
+        fName: "Hawaiian Pizza",
+        category: "pizza",
+        rating: 4.5,
+        price: 13,
+        img: "/pizza images/Hawaiian.jpg",
+        quantity: 1
+    },
+
+    {
+        id: 5,
+        fName: "Margherita Pizza",
+        category: "pizza",
+        rating: 4.5,
+        price: 11,
+        img: "/pizza images/Margherita.jpg",
+        quantity: 1
+    },
+
+    {
+        id: 6,
+        fName: "Meat Pizza",
+        category: "pizza",
+        rating: 4.5,
+        price: 13,
+        img: "/pizza images/Meat.jpg",
+        quantity: 1
+    },
+
+    {
+        id: 7,
+        fName: "Pepperoni Pizza",
+        category: "pizza",
+        rating: 4.5,
+        price: 14,
+        img: "/pizza images/Pepperoni.jpg",
+        quantity: 1
+    },
+
+    {
+        id: 8,
+        fName: "Supreme Pizza",
+        category: "pizza",
+        rating: 4.5,
+        price: 10,
+        img: "/pizza images/Supreme.jpg",
+        quantity: 1
+    },
+
+    {
+        id: 9,
+        fName: "The work's Pizza",
+        category: "pizza",
+        rating: 4.5,
+        price: 11,
+        img: "/pizza images/The work's.jpg",
+        quantity: 1
+    },
+
+    {
+        id: 10,
+        fName: "Veggie Pizza",
+        category: "pizza",
+        rating: 4.5,
+        price: 9,
+        img: "/pizza images/Veggie.jpg",
+        quantity: 1
+    },
+
+]
+
+export {foodItem};
